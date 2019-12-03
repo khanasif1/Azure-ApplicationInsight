98dc5d21e7ecca3b9c944a030f1960681972d4a0
Asif Khan <khanasif1@gmail.com>
2019-12-03 20:12:19 +1100

    Add project files.

---
+++ Azure-ApplicationInsight/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Azure_ApplicationInsight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DateTime _correlationTimeId = DateTime.UtcNow;
+            string _correlationId = Guid.NewGuid().ToString();
+            string _traceMessage = $"Starting sales service {DateTime.UtcNow.ToString()}";
+            AppInsightHelper.Instance.AppInsightInit(new AppInsightPayload
+            {
+                _operation = "SketcherSale",
+                _type = AppInsightLanguage.AppInsightTrace,
+                _payload = _traceMessage,
+                _correlationId = _correlationId,
+                _correlationTimeId = _correlationTimeId
+
+            });
+        }
+    }
+}
